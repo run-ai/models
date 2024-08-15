885b08de10b2e8e7d4a828bdf8cd3ae732c47f16
Yaron <yaron@run.ai>
2024-08-15 21:35:37 +0300

    arch

--- models/triton/server/build.sh
+++ models/triton/server/build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker build -t gcr.io/run-ai-demo/example-triton-server -f Dockerfile .
+docker build -t gcr.io/run-ai-demo/example-triton-server  --platform linux/amd64 -f Dockerfile .
